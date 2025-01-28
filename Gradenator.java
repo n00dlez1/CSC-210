f2376a9cc541e20dd773b559a425859182f74215
Noodles <143031441+n00dlez1@users.noreply.github.com>
2025-01-28 16:41:56 -0700

    Update Gradenator.java

--- Gradenator.java
+++ Gradenator.java
@@ -1,1 +1,12 @@
+//package before import statements
+package com.gradescope.gradenator;
 
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class gradenator {
+  public static void main(String[] args) throws FileNotFoundException{
+    
+  }
+}
